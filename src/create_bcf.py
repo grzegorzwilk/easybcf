1e45ac37a09114dbbf3977e018ebbb0931c68e6e
Boy d'Hont <boydhont@gmail.com>
2024-10-05 18:37:46 +0200

    Added vizualization Data

--- src/create_bcf.py
+++ src/create_bcf.py
@@ -2,7 +2,21 @@
 
 # first run "source venv/bin/activate"
 
-import bcf.v3.bcfxml as BCF
+import base64
+import os
+import uuid
+
+import bcf.v3.bcfxml as BCF # type: ignore
+import bcf.v3.model as MDL
+import bcf.v3.visinfo as VIS
+
+def prepare_snapshot(snapshot_filename: str) -> str:
+    """Reads a PNG file and returns its Base64 encoded string."""
+    full_snapshot_path = os.path.join(os.path.dirname(__file__), snapshot_filename)
+    
+    with open(full_snapshot_path, "rb") as image_file:
+        # Encode the image file to Base64 and return the string
+        return base64.b64encode(image_file.read()).decode('utf-8')
 
 bcf = BCF.BcfXml()
 new_bcf = bcf.create_new("LKJDLFJ")
@@ -15,5 +29,16 @@ new_topic = new_bcf.add_topic(
         topic_status="Open" # This might be redundant
     )
 
-new_bcf.save("test.bcf")
+snapshot = prepare_snapshot("snapshot.png")
+
+# Create a new VisualizationInfo object
+visualization_info = MDL.VisualizationInfo(
+    guid=str(uuid.uuid4()),  # Generate a new GUID
+    components=MDL.Components(),  # Initialize components as needed
+)
+
+new_viewpoint = VIS.VisualizationInfoHandler(visualization_info)
+new_viewpoint = new_topic.add_visinfo_handler(new_viewpoint, snapshot_filename=snapshot)
+
+new_bcf.save("test.bcf") # TODO activate
 print(new_bcf )
